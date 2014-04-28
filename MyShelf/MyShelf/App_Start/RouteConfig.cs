711b47eb46b224e891cf3f43500d2be44476a62d
Andreas Lengqvist <al223bn@student.lnu.se>
2014-04-28 17:26:42 +0200

    Påbörjat grunden i ASP.NET

---
+++ MyShelf/MyShelf/App_Start/RouteConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MyShelf
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            routes.MapPageRoute("Listing",
+            "",
+            "~/Pages/MoviePages/Listing.aspx");
+
+
+            routes.MapPageRoute("MovieCreator",
+                "Film/Ny",
+                "~/Pages/MoviePages/MovieCreator.aspx");
+
+
+
+            routes.MapPageRoute("GenreHandling",
+                "Genre/Hantering",
+                "~/Pages/MoviePages/GenreHandling.aspx");
+
+
+            routes.MapPageRoute("GenreCreator",
+                "Genre/Hantering/Ny",
+                "~/Pages/MoviePages/GenreCreator.aspx");
+
+
+            routes.MapPageRoute("MovieHandling",
+            "Film/{id}",
+            "~/Pages/MoviePages/MovieHandling.aspx");
+
+
+
+
+
+
+        }
+    }
+}
